2b726ae74865f96fea80507216e43a8d395a875d
abhay <the.abhaydeveloper@gmail.com>
2023-01-02 22:19:16 +0530

    hero section is complete with responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Navbar";
+import Section_1 from "./Section_1";
 
 const App = () =>{
   return (
     <>
       <div className="main_container">
           <Navbar/>
+          <Section_1/>
       </div>
     </>
   );
---
+++ src/Section_1.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Section_1.css";
+
+const Section_1 = () => {
+    return (
+        <>
+            <div className="section_1_main_container">
+                <div className="section_1_text_and_button">
+                    <div className="heading">
+                        <h1 className="section_1_heading index">Time to Get Your House Clean and in Order</h1>
+                    </div>
+                    <div className="pera">
+                        <p className="section_1_pera index">To have good air quality is not as simple as moving away from the city to a quiet suburb or rural environment</p>
+                    </div>
+                    <div className="button">
+                        <button className="section_1_button index">Read Artical</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Section_1;
